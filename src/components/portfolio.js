be042af233e13a27f90c0184bf4f406d366f4c8d
JessiStrattonWeber <jessicastratton1@mail.weber.edu>
2024-08-11 20:45:46 -0600

    Added project details

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -6,7 +6,7 @@ function Portfolio() {
     return (
         <div className="container">
             <h2>My Projects</h2>
-            <a className="btn btn-primary" href='https://github.com/JessiStrattonWeber/CS3750-Portfolio'>Clone Code</a>
+            <a className="btn btn-primary" href='https://github.com/JessiStrattonWeber/CS3750-Portfolio'>View Code</a>
             <div className="row">
                 {projects.map((project, index) => (
                     <div className="col-md-4 d-flex" key={index}>
@@ -15,7 +15,7 @@ function Portfolio() {
                             <div className="card-body">
                                 <h5 className="card-title">{project.title}</h5>
                                 <p className="card-text">{project.description}</p>
-                                <Link to={`/project/${project.id}`} className="btn btn-primary">View Details</Link>
+                                <Link to={`/project/${project.id}`} className="btn btn-secondary">View Details</Link>
                             </div>
                         </div>
                     </div>
